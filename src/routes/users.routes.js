9615df897050f26e87cd04f8e9d274e32110c8bb
iago <iago.romao5@gmail.com>
2023-05-22 00:27:34 -0400

    first commit

---
+++ src/routes/users.routes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController.js");
+
+const usersRoutes = Router();
+
+function myMiddleware(request, response, next,){
+    console.log("Voce passou pelo middleware!")
+
+    if (!request.body.isAdmin) {
+        return response.json({ message: "user unauthorized" })
+    } 
+
+    next();
+}
+
+const usersController = new UsersController();
+
+usersRoutes.post("/", myMiddleware, usersController.create)
+
+module.exports = usersRoutes;
